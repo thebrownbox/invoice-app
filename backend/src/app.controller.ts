fbb7e33797de81be5081ab91057ca43d3fff9cf2
Cong Hoang <cong.hoang@sleek.com>
2025-08-06 20:36:23 +0700

    a

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getInvoice(@Res() res: Response) {
+    const buffer = await this.appService.generateInvoice();
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'inline; filename=invoice.pdf',
+    });
+    res.send(buffer);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import * as PdfPrinter from 'pdfmake';
+import * as getStream from 'get-stream';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async generateInvoice(): Promise<Buffer> {
+    const fonts = {
+      Roboto: {
+        normal: 'fonts/Roboto-Regular.ttf',
+        bold: 'fonts/Roboto-Medium.ttf',
+        italics: 'fonts/Roboto-Italic.ttf',
+        bolditalics: 'fonts/Roboto-MediumItalic.ttf',
+      },
+    };
+    const printer = new PdfPrinter(fonts);
+
+    const docDefinition = {
+      content: [
+        { text: 'INVOICE', style: 'header' },
+        {
+          table: {
+            widths: ['*', '*', '*', '*'],
+            body: [
+              ['Item', 'Quantity', 'Unit Price', 'Total'],
+              ['Widget A', '2', '$10', '$20'],
+              ['Widget B', '1', '$15', '$15'],
+              [{ text: 'Total', colSpan: 3 }, {}, {}, '$35'],
+            ],
+          },
+        },
+      ],
+    };
+
+    const pdfDoc = printer.createPdfKitDocument(docDefinition);
+    pdfDoc.end();
+    return await getStream.buffer(pdfDoc);
   }
 }
