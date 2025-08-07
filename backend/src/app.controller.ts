e0bc8924a3fe7dd0810edb33de75ba206f3fde45
Cong Hoang <cong.hoang@sleek.com>
2025-08-07 17:20:09 +0700

    a

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import * as html_to_pdf from 'html-pdf-node';
 
 @Controller()
 export class AppController {
@@ -14,5 +15,28 @@ export class AppController {
       'Content-Disposition': 'inline; filename=invoice.pdf',
     });
     res.send(buffer);
+  }
+
+  @Get('v1')
+  async getInvoiceByPuppeteer(@Res() res: Response) {
+    const options = { format: 'A4' };
+    const file = { content: '<h1>Welcome to html-pdf-node</h1>' };
+
+    html_to_pdf
+      .generatePdf(file, options)
+      .then((output) => {
+        res.set({
+          'Content-Type': 'application/pdf',
+          'Content-Disposition': 'inline; filename=invoice.pdf',
+        });
+        res.send(output);
+      })
+      .catch((err) => {
+        console.log('Error:-', err);
+      })
+      .finally(() => {
+        // res.send('Finished');
+        console.log('Finished');
+      });
   }
 }
